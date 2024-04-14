aa5748dd72ae2207a9413ea8056b26a7c19fc9a2
Yang, Bo <bo.yang@protonbase.io>
2024-04-14 06:11:42 +0000

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    poetry-add-requirements-txt = {
+      url = "github:tddschn/poetry-add-requirements.txt";
+      flake = false;
+    };
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = inputs: {
+    packages = inputs.nixpkgs.lib.attrsets.genAttrs inputs.nixpkgs.lib.systems.flakeExposed (system: {
+      default = 
+        (inputs.poetry2nix.lib.mkPoetry2Nix {
+          pkgs = inputs.nixpkgs.legacyPackages.${system};
+        }).mkPoetryApplication {
+          projectDir = inputs.poetry-add-requirements-txt;
+        };
+    });
+  };
+}
+

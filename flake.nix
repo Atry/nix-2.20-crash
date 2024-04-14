92a776cd79e8a152814053c66d6adabb0af6ac23
Yang, Bo <bo.yang@protonbase.io>
2024-04-14 06:41:05 +0000

    Specify revision

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     poetry-add-requirements-txt = {
-      url = "github:tddschn/poetry-add-requirements.txt";
+      url = "github:tddschn/poetry-add-requirements.txt/710dde128b3746e7269e423f46f1e0e432c47043";
       flake = false;
     };
     poetry2nix = {
